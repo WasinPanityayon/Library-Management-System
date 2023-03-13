11521f8f1027d7b808e122fabbf3ae47c8fc676a
WasinPanityayon <wasinpanityayon08@gmail.com>
2023-03-13 17:38:58 +0700

    GET POST PATCH DELETE Users

---
+++ Library Management System [BACKEND]/controllers/usersController.js
@@ -0,0 +1,105 @@
+const User = require('../models/User')
+const Book = require('../models/Book')
+const bcrypt = require('bcrypt')
+
+const getAllUsers = async (req, res) => {
+    const users = await User.find().select('-password').lean()
+
+    if (!users?.length) {
+        return res.status(400).json({ message: 'No users found' })
+    }
+
+    res.json(users)
+}
+
+const createNewUser = async (req, res) => {
+    const { firstname, surname, username, password, roles } = req.body
+
+    if (!firstname || !surname || !username || !password) {
+        return res.status(400).json({ message: 'All fields are required' })
+    }
+
+    const duplicate = await User.findOne({ username }).collation({ locale: 'en', strength: 2 }).lean().exec()
+
+    if (duplicate) {
+        return res.status(409).json({ message: 'Duplicate username' })
+    }
+
+    const hashedPwd = await bcrypt.hash(password, 10)
+
+    const userObject = (!Array.isArray(roles) || !roles.length)
+        ? { firstname, surname, username, "password": hashedPwd }
+        : { firstname, surname, username, "password": hashedPwd, roles }
+
+    const user = await User.create(userObject)
+
+    if (user) {
+        res.status(201).json({ message: `New user ${username} created` })
+    } else {
+        res.status(400).json({ message: 'Invalid user data received' })
+    }
+}
+
+const updateUser = async (req, res) => {
+    const { id, username, roles, active, password } = req.body
+
+    if (!id || !username || !Array.isArray(roles) || !roles.length || typeof active !== 'boolean') {
+        return res.status(400).json({ message: 'All fields except password are required' })
+    }
+
+    const user = await User.findById(id).exec()
+
+    if (!user) {
+        return res.status(400).json({ message: 'User not found' })
+    }
+
+    const duplicate = await User.findOne({ username }).collation({ locale: 'en', strength: 2 }).lean().exec()
+
+    if (duplicate && duplicate?._id.toString() !== id) {
+        return res.status(409).json({ message: 'Duplicate username' })
+    }
+
+    user.username = username
+    user.roles = roles
+    user.active = active
+
+    if (password) {
+        user.password = await bcrypt.hash(password, 10)
+    }
+
+    const updatedUser = await user.save()
+
+    res.json({ message: `${updatedUser.username} updated` })
+}
+
+const deleteUser = async (req, res) => {
+    const { id } = req.body
+
+    if (!id) {
+        return res.status(400).json({ message: 'User ID Required' })
+    }
+
+    const book = await Book.findOne({ user: id }).lean().exec()
+    if (book) {
+        return res.status(400).json({ message: 'User has assigned books' })
+    }
+
+    const user = await User.findById(id).exec()
+
+    if (!user) {
+        return res.status(400).json({ message: 'User not found' })
+    }
+
+    const result = await user.deleteOne()
+
+    const reply = `Username ${result.username} with ID ${result._id} deleted`
+
+    res.json(reply)
+}
+
+module.exports = {
+    getAllUsers,
+    createNewUser,
+    updateUser,
+    deleteUser
+}
