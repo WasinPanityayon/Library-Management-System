cef3205ba5a45653aef2a5eaf25ea61e4b2d5a6c
PAKAPON471 <6310110345@psu.ac.th>
2023-03-13 15:37:03 +0700

    edit homepage content with my design no css

--- Library Management System [FRONTEND]/src/components/HomepageFooter.js
+++ Library Management System [FRONTEND]/src/components/HomepageFooter.js
@@ -1,5 +1,46 @@
+import { useEffect, useState } from "react";
+
 const HomepageFooter = () => {
-  
+
+  const [realtimeToday, setRealtimeToday] = useState(
+    new Intl.DateTimeFormat("en-US", {
+      dateStyle: "full",
+      timeStyle: "long",
+    }).format(new Date())
+  );
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setRealtimeToday(
+        new Intl.DateTimeFormat("en-US", {
+          dateStyle: "full",
+          timeStyle: "long",
+        }).format(new Date())
+      );
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const content = (
+    <section>
+      <section className="homepage_footer_img">
+        <img
+          src="https://www.skoolbeep.com/blog/wp-content/uploads/2020/12/WHAT-IS-THE-PURPOSE-OF-A-LIBRARY-MANAGEMENT-SYSTEM-min.png"
+          alt="Library Img1"
+        />
+      </section>
+      <footer className="homepage_footer">
+        <nav className="homepage_footer_nav">
+          <p className="text">Current User: </p>
+          <p className="text">Status: </p>
+        </nav>
+        <nav className="homepage_footer_nav">
+          <p className="text">{realtimeToday}</p>
+        </nav>
+      </footer>
+    </section>
+  );
+  return content;
 };
 
 export default HomepageFooter;
--- Library Management System [FRONTEND]/src/components/HomepageHeader.js
+++ Library Management System [FRONTEND]/src/components/HomepageHeader.js
@@ -1,4 +1,122 @@
+import { faArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useNavigate } from "react-router-dom"
+
 const HomepageHeader = () => {
+    const navigate = useNavigate()
+
+    const onbooksClicked = () => navigate('/books')
+    const ontransectionsClicked = () => navigate('/books')
+    const onsettingsClicked = () => navigate('/settings')
+    const onusersClicked = () => navigate('/users')
+    const onsigninClicked = () => navigate('/signin')
+    const onsignupClicked = () => navigate('/signup')
+    const onsignoutClicked = () => navigate('/signout')
+
+    let booksButton = (
+        <button
+            className="button_og"
+            title="Books"
+            onClick={onbooksClicked}
+        >
+            Books
+        </button>
+    )
+
+    let transectionsButton = (
+        <button
+            className="button_og"
+            title="Transections"
+            onClick={ontransectionsClicked}
+        >
+            Transections
+        </button>
+    )
+
+    let settingsButton = (
+        <button
+            className="button_og"
+            title="Settings"
+            onClick={onsettingsClicked}
+        >
+            Settings
+        </button>
+    )
+
+    let usersButton = (
+        <button
+            className="button_og"
+            title="Users"
+            onClick={onusersClicked}
+        >
+            Users
+        </button>
+    )
+
+    let signupButton = (
+        <button
+            className="button_og"
+            title="SIGN UP"
+            onClick={onsignupClicked}
+        >
+            SIGN UP
+        </button>
+    )
+
+
+    let signinButton =  (
+        <button
+            className="button_signin"
+            title="Sign In"
+            onClick={onsigninClicked}
+        >
+            SIGN IN
+        </button>
+    )
+
+    let signoutButton = (
+        <button
+            className="icon_button"
+            title="Signout"
+            onClick={onsignoutClicked}
+        >
+            <FontAwesomeIcon icon={faArrowRightFromBracket} />
+        </button>
+    )
+
+    let buttonContentLeft = (
+        <>
+            {booksButton}
+            {transectionsButton}
+            {settingsButton}
+            {usersButton}
+        </>
+    )
     
+    let buttonContentRight = (
+        <>
+            {signupButton}
+            {signinButton}
+            {signoutButton}
+        </>
+    )
+
+    const content = (
+        <>
+            <header className="homepage_header">
+                <div className={`homepage_header_container`}>
+                    <nav className="homepage_header_nav">
+                        {buttonContentLeft}
+                    </nav>
+                    <nav className="homepage_header_nav">
+                        {buttonContentRight}
+                    </nav>
+                </div>
+            </header>
+        </>
+    )
+
+    return content
+
 }
 export default HomepageHeader
