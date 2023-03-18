a65318c245951b166f6a59f9615939aaa0f70bfe
PAKAPON471 <6310110345@psu.ac.th>
2023-03-18 11:56:00 +0700

    get all Book from Backend

---
+++ Library Management System [FRONTEND]/src/features/books/Book.js
@@ -0,0 +1,43 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPenToSquare } from "@fortawesome/free-solid-svg-icons"
+import { useNavigate } from 'react-router-dom'
+
+import { useSelector } from 'react-redux'
+import { selectBookById } from './booksApiSlice'
+
+const Book = ({ bookId }) => {
+
+    const book = useSelector(state => selectBookById(state, bookId))
+
+    const navigate = useNavigate()
+
+    if (book) {
+        const handleEdit = () => navigate(`/settings/bookslist/${bookId}`)
+
+        return (
+            <tr className="table__row">
+                <td className="table__cell book__title">{book.ticket}</td>
+                <td className="table__cell book__title">
+                    <img className='book__img'
+                        src={book.imagebook}
+                        alt={book.imagebook}
+                    />
+                </td>
+                <td className="table__cell book__title">{book.title}</td>
+                <td className="table__cell book__title">{book.author}</td>
+                <td className="table__cell book__title">{book.isbn}</td>
+                <td className="table__cell">
+                    <button
+                        className="icon-button table__button"
+                        onClick={handleEdit}
+                    >
+                        <FontAwesomeIcon icon={faPenToSquare} />
+                    </button>
+                </td>
+            </tr>
+        )
+
+    } else return null
+}
+
+export default Book
--- Library Management System [FRONTEND]/src/features/books/BooksList.js
+++ Library Management System [FRONTEND]/src/features/books/BooksList.js
@@ -1,4 +1,48 @@
+import { useGetBooksQuery } from "./booksApiSlice"
+import Book from "./Book"
+
 const BooksList = () => {
+    const {
+        data: books,
+        isLoading,
+        isSuccess,
+        isError,
+        error
+    } = useGetBooksQuery()
+    
+    let content
+
+    if (isLoading) content = <p>Loading...</p>
+
+    if (isError) {
+        content = <p className="errmsg">{error?.data?.message}</p>
+    }
+
+    if (isSuccess) {
+        const { ids } = books
+
+        const tableContent = ids?.length && ids.map(bookId => <Book key={bookId} bookId={bookId} />)
+
+        content = (
+            <table className="table_book">
+                <thead className="table__thead">
+                    <tr>
+                        <th scope="col" className="table__th book__edit">Book No.</th>
+                        <th scope="col" className="table__th book__title">Img</th>
+                        <th scope="col" className="table__th book__title">Title</th>
+                        <th scope="col" className="table__th book__status">author</th>
+                        <th scope="col" className="table__th book__status">isbn</th>
+                        <th scope="col" className="table__th book__edit">Edit</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {tableContent}
+                </tbody>
+            </table>
+        )
+    }
+
+    return content
     
 }
 export default BooksList
